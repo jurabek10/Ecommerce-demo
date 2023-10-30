fd18f9d8f8aacf65dfb7d4f005face44331ef4f3
jurabek10 <samiev.jurabek@bk.ru>
2023-10-30 15:17:01 +0900

    Update active showcase on thumbnail click

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+const modifiers = {
+    imgThumbnailActive: "img-showcase_thumbnail--active"
+};
+
+
+
+
+// Shopping-cart-modal
 const elSiteHeaderCartLink = document.querySelector(".js-site-header-cart-link");
 const elSiteHeaderCartModal = document.querySelector(".site-header__cart-modal");
 
@@ -9,3 +17,25 @@ if (elSiteHeaderCartLink) {
         elSiteHeaderCartModal.classList.toggle("site-header__cart-modal--open")
     });
 }
+
+
+// Image showcase
+const elImgShowcaseActiveImg = document.querySelector(".img-showcase__active-img");
+const elsImgShowcaseThumbnailButton = document.querySelectorAll(".js-img-showcase-thumbnail-button");
+const elsImgThumbnail = document.querySelectorAll(".img-showcase_thumbnail");
+
+elsImgShowcaseThumbnailButton.forEach(function (elButton) {
+    elButton.addEventListener("click", function () {
+        // Remove active state from all buttons
+        elsImgThumbnail.forEach(function (elImgThumbnail) {
+            elImgThumbnail.classList.remove(modifiers.imgThumbnailActive);
+        });
+
+        // Add active state to click button
+        elButton.parentElement.classList.add(modifiers.imgThumbnailActive);
+
+        // Update active img accordingly
+        elImgShowcaseActiveImg.src = elButton.dataset.imgShowcaseBig;
+        elImgShowcaseActiveImg.srcset = `${elButton.dataset.imgShowcaseBig} 1x ${elButton.dataset.imgShowcaseBigRetina}2x`;
+        });
+    });
